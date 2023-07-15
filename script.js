60c58faf58649a420bd4ceef5dcc2d616f98f43c
Brett Tanner <bretttanner171@gmail.com>
2023-07-15 12:02:03 +0900

    Add function to display books in library

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
-let library = [];
+const library = [];
+const bookCase = document.getElementById("bookcase");
 class Book {
     constructor(title, author, pages, read) {
         this.title = title;
@@ -11,6 +12,35 @@ class Book {
 function addBook(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     library.push(newBook);
-    console.log(library);
+    showLibrary();
 }
-addBook("Pizza", "Pizza Lover", 10, false);
+function createDisplayBook(book) {
+    const displayBook = document.createElement("div");
+    displayBook.classList.add("book");
+    const title = document.createElement("h3");
+    title.innerText = book.title;
+    displayBook.appendChild(title);
+    const author = document.createElement("h4");
+    author.innerText = `By: ${book.author}`;
+    displayBook.appendChild(author);
+    const info = document.createElement("div");
+    info.classList.add("info");
+    const pages = document.createElement("p");
+    pages.innerText = book.pages.toString();
+    info.appendChild(pages);
+    const read = document.createElement("p");
+    read.innerText = book.read ? "Read" : "Unread";
+    info.appendChild(read);
+    displayBook.appendChild(info);
+    return displayBook;
+}
+function showLibrary() {
+    bookCase.innerHTML = "";
+    for (const book of library) {
+        const displayBook = createDisplayBook(book);
+        bookCase === null || bookCase === void 0 ? void 0 : bookCase.appendChild(displayBook);
+    }
+}
+for (let i = 0; i < 10; i++) {
+    addBook("Pizza", "Pizza Lover", 10, false);
+}
--- script.ts
+++ script.ts
@@ -1,4 +1,5 @@
-let library: Book[] = [];
+const library: Book[] = [];
+const bookCase = document.getElementById("bookcase")!;
 
 class Book {
   title: string;
@@ -17,7 +18,44 @@ class Book {
 function addBook(title: string, author: string, pages: number, read: boolean) {
   const newBook = new Book(title, author, pages, read);
   library.push(newBook);
-  console.log(library);
+  showLibrary();
 }
 
-addBook("Pizza", "Pizza Lover", 10, false);
+function createDisplayBook(book: Book) {
+  // Create the book container
+  const displayBook = document.createElement("div");
+  displayBook.classList.add("book");
+
+  // Create the heading info
+  const title = document.createElement("h3");
+  title.innerText = book.title;
+  displayBook.appendChild(title);
+  const author = document.createElement("h4");
+  author.innerText = `By: ${book.author}`;
+  displayBook.appendChild(author);
+
+  // Create the detailed info
+  const info = document.createElement("div");
+  info.classList.add("info");
+  const pages = document.createElement("p");
+  pages.innerText = book.pages.toString();
+  info.appendChild(pages);
+  const read = document.createElement("p");
+  read.innerText = book.read ? "Read" : "Unread";
+  info.appendChild(read);
+  displayBook.appendChild(info);
+
+  return displayBook;
+}
+
+function showLibrary() {
+  bookCase.innerHTML = "";
+  for (const book of library) {
+    const displayBook: HTMLDivElement = createDisplayBook(book);
+    bookCase?.appendChild(displayBook);
+  }
+}
+
+for (let i = 0; i < 10; i++) {
+  addBook("Pizza", "Pizza Lover", 10, false);
+}
